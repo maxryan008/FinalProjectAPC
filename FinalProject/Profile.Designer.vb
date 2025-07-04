55a9f06ec65bd1cb91773a204b733bb63509a800
maxryan008 <maxryan008@gmail.com>
2025-07-04 10:42:06 +1000

    l

--- FinalProject/Profile.Designer.vb
+++ FinalProject/Profile.Designer.vb
@@ -23,7 +23,18 @@ Partial Class Profile
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         panelMain = New Panel()
-        Label1 = New Label()
+        lblAccountType = New Label()
+        lblAge = New Label()
+        lblEmail = New Label()
+        lblUsername = New Label()
+        lblSurname = New Label()
+        lblName = New Label()
+        lblAccountTypeHeading = New Label()
+        lblAgeHeading = New Label()
+        lblEmailHeading = New Label()
+        lblUsernameHeading = New Label()
+        lblSurnameHeading = New Label()
+        lblNameHeading = New Label()
         panelHeader = New Panel()
         btnBack = New Button()
         lblTitle = New Label()
@@ -34,21 +45,131 @@ Partial Class Profile
         ' panelMain
         ' 
         panelMain.BackColor = Color.SeaShell
-        panelMain.Controls.Add(Label1)
+        panelMain.Controls.Add(lblAccountType)
+        panelMain.Controls.Add(lblAge)
+        panelMain.Controls.Add(lblEmail)
+        panelMain.Controls.Add(lblUsername)
+        panelMain.Controls.Add(lblSurname)
+        panelMain.Controls.Add(lblName)
+        panelMain.Controls.Add(lblAccountTypeHeading)
+        panelMain.Controls.Add(lblAgeHeading)
+        panelMain.Controls.Add(lblEmailHeading)
+        panelMain.Controls.Add(lblUsernameHeading)
+        panelMain.Controls.Add(lblSurnameHeading)
+        panelMain.Controls.Add(lblNameHeading)
         panelMain.ImeMode = ImeMode.NoControl
         panelMain.Location = New Point(-2, 67)
         panelMain.Name = "panelMain"
         panelMain.Size = New Size(804, 385)
         panelMain.TabIndex = 3
         ' 
-        ' Label1
+        ' lblAccountType
         ' 
-        Label1.AutoSize = True
-        Label1.Location = New Point(14, 12)
-        Label1.Name = "Label1"
-        Label1.Size = New Size(41, 15)
-        Label1.TabIndex = 6
-        Label1.Text = "Label1"
+        lblAccountType.AutoSize = True
+        lblAccountType.Location = New Point(103, 87)
+        lblAccountType.Name = "lblAccountType"
+        lblAccountType.Size = New Size(10, 15)
+        lblAccountType.TabIndex = 17
+        lblAccountType.Text = "."
+        ' 
+        ' lblAge
+        ' 
+        lblAge.AutoSize = True
+        lblAge.Location = New Point(43, 72)
+        lblAge.Name = "lblAge"
+        lblAge.Size = New Size(10, 15)
+        lblAge.TabIndex = 16
+        lblAge.Text = "."
+        ' 
+        ' lblEmail
+        ' 
+        lblEmail.AutoSize = True
+        lblEmail.Location = New Point(51, 57)
+        lblEmail.Name = "lblEmail"
+        lblEmail.Size = New Size(10, 15)
+        lblEmail.TabIndex = 15
+        lblEmail.Text = "."
+        ' 
+        ' lblUsername
+        ' 
+        lblUsername.AutoSize = True
+        lblUsername.Location = New Point(77, 42)
+        lblUsername.Name = "lblUsername"
+        lblUsername.Size = New Size(10, 15)
+        lblUsername.TabIndex = 14
+        lblUsername.Text = "."
+        ' 
+        ' lblSurname
+        ' 
+        lblSurname.AutoSize = True
+        lblSurname.Location = New Point(71, 27)
+        lblSurname.Name = "lblSurname"
+        lblSurname.Size = New Size(10, 15)
+        lblSurname.TabIndex = 13
+        lblSurname.Text = "."
+        ' 
+        ' lblName
+        ' 
+        lblName.AutoSize = True
+        lblName.Location = New Point(55, 12)
+        lblName.Name = "lblName"
+        lblName.Size = New Size(10, 15)
+        lblName.TabIndex = 12
+        lblName.Text = "."
+        ' 
+        ' lblAccountTypeHeading
+        ' 
+        lblAccountTypeHeading.AutoSize = True
+        lblAccountTypeHeading.Location = New Point(14, 87)
+        lblAccountTypeHeading.Name = "lblAccountTypeHeading"
+        lblAccountTypeHeading.Size = New Size(83, 15)
+        lblAccountTypeHeading.TabIndex = 11
+        lblAccountTypeHeading.Text = "Account Type:"
+        ' 
+        ' lblAgeHeading
+        ' 
+        lblAgeHeading.AutoSize = True
+        lblAgeHeading.Location = New Point(14, 72)
+        lblAgeHeading.Name = "lblAgeHeading"
+        lblAgeHeading.Size = New Size(31, 15)
+        lblAgeHeading.TabIndex = 10
+        lblAgeHeading.Text = "Age:"
+        ' 
+        ' lblEmailHeading
+        ' 
+        lblEmailHeading.AutoSize = True
+        lblEmailHeading.Location = New Point(14, 57)
+        lblEmailHeading.Name = "lblEmailHeading"
+        lblEmailHeading.Size = New Size(39, 15)
+        lblEmailHeading.TabIndex = 9
+        lblEmailHeading.Text = "Email:"
+        ' 
+        ' lblUsernameHeading
+        ' 
+        lblUsernameHeading.AutoSize = True
+        lblUsernameHeading.Location = New Point(14, 42)
+        lblUsernameHeading.Name = "lblUsernameHeading"
+        lblUsernameHeading.Size = New Size(63, 15)
+        lblUsernameHeading.TabIndex = 8
+        lblUsernameHeading.Text = "Username:"
+        ' 
+        ' lblSurnameHeading
+        ' 
+        lblSurnameHeading.AutoSize = True
+        lblSurnameHeading.Location = New Point(14, 27)
+        lblSurnameHeading.Name = "lblSurnameHeading"
+        lblSurnameHeading.Size = New Size(57, 15)
+        lblSurnameHeading.TabIndex = 7
+        lblSurnameHeading.Text = "Surname:"
+        ' 
+        ' lblNameHeading
+        ' 
+        lblNameHeading.AutoSize = True
+        lblNameHeading.Location = New Point(14, 12)
+        lblNameHeading.Name = "lblNameHeading"
+        lblNameHeading.Size = New Size(42, 15)
+        lblNameHeading.TabIndex = 6
+        lblNameHeading.Text = "Name:"
         ' 
         ' panelHeader
         ' 
@@ -101,5 +222,16 @@ Partial Class Profile
     Friend WithEvents panelHeader As Panel
     Friend WithEvents lblTitle As Label
     Friend WithEvents btnBack As Button
-    Friend WithEvents Label1 As Label
+    Friend WithEvents lblNameHeading As Label
+    Friend WithEvents lblSurnameHeading As Label
+    Friend WithEvents lblUsernameHeading As Label
+    Friend WithEvents lblEmailHeading As Label
+    Friend WithEvents lblAgeHeading As Label
+    Friend WithEvents lblAccountTypeHeading As Label
+    Friend WithEvents lblAccountType As Label
+    Friend WithEvents lblAge As Label
+    Friend WithEvents lblEmail As Label
+    Friend WithEvents lblUsername As Label
+    Friend WithEvents lblSurname As Label
+    Friend WithEvents lblName As Label
 End Class
--- FinalProject/Profile.vb
+++ FinalProject/Profile.vb
@@ -6,7 +6,19 @@
     End Sub
 
     '' Every time form is reloaded / reshown / visibility changed
-    Private Sub formWindowOpenedOrClosed(sender As Object, e As EventArgs) Handles Me.VisibleChanged
-        Label1.Text = MainMenu.session.data.strUsername
+    ' refresh all user details
+    Private Sub FormWindowOpenedOrClosed(sender As Object, e As EventArgs) Handles Me.VisibleChanged
+        lblName.Text = MainMenu.session.data.strName
+        lblSurname.Text = MainMenu.session.data.strSurname
+        lblUsername.Text = MainMenu.session.data.strUsername
+        lblEmail.Text = MainMenu.session.data.strEmail
+        lblAge.Text = MainMenu.session.data.intAge
+        lblAccountType.Text = MainMenu.session.data.strAccountType
+    End Sub
+
+    ' Redirect to main menu when form is closed
+    Private Sub Form_Closed(sender As Object, e As EventArgs) Handles MyBase.FormClosed
+        'Already closed so dont close again otherwise crash will occur
+        FormRedirectionManager.redirect(Me, MainMenu)
     End Sub
 End Class
